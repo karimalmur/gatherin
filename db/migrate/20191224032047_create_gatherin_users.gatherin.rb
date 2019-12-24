c978bfc3830ab72838f03f6f83fd5e2cabc57b71
karim <karimit@live.com>
2019-12-24 05:21:32 +0200

    Copies & runs user creation migration

---
+++ db/migrate/20191224032047_create_gatherin_users.gatherin.rb
@@ -0,0 +1,14 @@
+# This migration comes from gatherin (originally 20191224012314)
+class CreateGatherinUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :gatherin_users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.date   :birthdate
+      t.string :gender
+
+      t.timestamps
+    end
+  end
+end
