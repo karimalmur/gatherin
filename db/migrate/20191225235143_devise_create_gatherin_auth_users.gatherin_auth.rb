b6f1cc15b4b28c1ef0acbe2d2dcddf5311b0ce9f
karim <karimit@live.com>
2019-12-26 04:24:44 +0200

    Installs auth's devise related migrations

---
+++ db/migrate/20191225235143_devise_create_gatherin_auth_users.gatherin_auth.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+# This migration comes from gatherin_auth (originally 20191225233944)
+
+class DeviseCreateGatherinAuthUsers < ActiveRecord::Migration[6.0]
+  def change
+    ## Database authenticatable
+    change_column :gatherin_users, :email, :string, null: false, default: ""
+    add_column :gatherin_users, :encrypted_password, :string, null: false, default: ""
+
+    ## Recoverable
+    add_column :gatherin_users, :reset_password_token, :string
+    add_column :gatherin_users, :reset_password_sent_at, :datetime
+
+    ## Rememberable
+    add_column :gatherin_users, :remember_created_at, :datetime
+
+    ## Confirmable
+    add_column :gatherin_users, :confirmation_token, :string
+    add_column :gatherin_users, :confirmed_at, :datetime
+    add_column :gatherin_users, :confirmation_sent_at, :datetime
+
+    ## Lockable
+    # add_column :gatherin_users, :failed_attempts, :integer, default: 0, null: false # Only if lock strategy is :failed_attempts
+    add_column :gatherin_users, :unlock_token, :string # Only if unlock strategy is :email or :both
+    add_column :gatherin_users, :locked_at, :datetime
+
+    add_index :gatherin_users, :email,                unique: true
+    add_index :gatherin_users, :reset_password_token, unique: true
+    add_index :gatherin_users, :confirmation_token,   unique: true
+    add_index :gatherin_users, :unlock_token,         unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_25_034434) do
+ActiveRecord::Schema.define(version: 2019_12_25_235143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,11 +18,24 @@ ActiveRecord::Schema.define(version: 2019_12_25_034434) do
   create_table "gatherin_users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "email"
+    t.string "email", default: "", null: false
     t.date "birthdate"
     t.string "gender"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.index ["confirmation_token"], name: "index_gatherin_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_gatherin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_gatherin_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_gatherin_users_on_unlock_token", unique: true
   end
 
   create_table "oauth_access_grants", force: :cascade do |t|
