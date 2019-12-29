fc4ea6257b2d033828b8cf280c34da4211033b52
karim <karimit@live.com>
2019-12-29 22:41:10 +0200

    Removes tzinfo-data
    See https://github.com/tzinfo/tzinfo-data/issues/12#issuecomment-279554001

--- shared_dependencies.rb
+++ shared_dependencies.rb
@@ -15,6 +15,3 @@ group :development, :test do
   gem "pry-rails"
   gem "rspec-rails", "~> 3.9"
 end
-
-# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem "tzinfo-data", platforms: %i[mingw mswin x64_mingw jruby]
