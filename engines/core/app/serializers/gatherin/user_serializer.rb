338518a8b67ee4d7d88817ddf9619f6cc5bad645
karim <karimit@live.com>
2019-12-30 02:28:19 +0200

    [core] Adds avatar url to user serializer

--- engines/core/app/serializers/gatherin/user_serializer.rb
+++ engines/core/app/serializers/gatherin/user_serializer.rb
@@ -3,5 +3,11 @@ module Gatherin
     include ::FastJsonapi::ObjectSerializer
 
     attributes :first_name, :last_name, :name, :email, :gender, :created_at, :updated_at
+
+    attribute :avatar do |user|
+      if user.avatar.attached?
+        Rails.application.routes.url_helpers.rails_blob_path(user.avatar, only_path: true)
+      end
+    end
   end
 end
